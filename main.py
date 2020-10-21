19dd048e86423ac10d7d9fc63ed9ebea2eb5f231
Minigrim0 <grimauflorent@gmail.com>
2020-10-21 12:56:42 +0200

    [Add] responds "lu" to all message in concerned channel

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import discord
+import os
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        # channel = client.get_channel(755154529401438344)
+        # await channel.send('Hello')
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
+        if str(message.author) != "Bert#4668":
+            await message.channel.send('Lu')
 
+
+API_KEY = os.environ.get("API_KEY", "")
+if API_KEY == "":
+    raise Exception("You must specify an API key")
 
 client = MyClient()
-client.run('my token goes here')
+client.run(API_KEY)
