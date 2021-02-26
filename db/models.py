96b7fa465ea6d299f40cc79d4f638b328c11098d
Minigrim0 <grimauflorent@gmail.com>
2021-02-26 11:01:17 +0100

    [Upd] made reminder name unique + added server handling

--- db/models.py
+++ db/models.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 
 
 class Reminder(models.Model):
-    name = models.CharField(max_length=150, default="none")
+    name = models.CharField(max_length=150, default="none", unique=True)
     start_time = models.DateTimeField(null=True)
     duration = models.TimeField(null=True)
+    guild = models.IntegerField(default=0)
     role_to_remind = models.CharField(max_length=150, null=True)
 
     @property
@@ -16,8 +17,4 @@ class Reminder(models.Model):
 
     @property
     def serialized(self):
-        return json.dumps({
-            "ID": self.id,
-            "Name": self.name,
-            "Time": self.time
-        })
+        return json.dumps({"ID": self.id, "Name": self.name, "Time": self.time})
