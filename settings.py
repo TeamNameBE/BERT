31e161c236d4007ea7f08586947e360df3e7978b
Minigrim0 <grimauflorent@gmail.com>
2021-03-26 23:34:31 +0100

    [Upd] database

--- settings.py
+++ settings.py
@@ -4,9 +4,13 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    'default': {
+        "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
