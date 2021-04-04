9d9e9432265ed15f8fe66369eed0e09ad15ebcc3
Minigrim0 <grimauflorent@gmail.com>
2021-04-04 10:35:50 +0200

    [Upd] positive big integer field

--- db/models.py
+++ db/models.py
@@ -8,7 +8,7 @@ class Reminder(models.Model):
     name = models.CharField(max_length=150, default="none", unique=True)
     start_time = models.DateTimeField(null=True)
     duration = models.TimeField(null=True)
-    guild = models.CharField(max_length=128, default=0)
+    guild = models.PositiveBigIntegerField(default=0)
     channel = models.CharField(max_length=128, default=0)
     role_to_remind = models.TextField(null=True)
     advertised = models.BooleanField(default=False)
