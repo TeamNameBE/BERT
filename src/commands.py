a33ec34245dbc14323d0b49204cb461e090df3d7
Minigrim0 <grimauflorent@gmail.com>
2021-06-30 15:04:07 +0200

    [Upd] command file

--- src/commands.py
+++ src/commands.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import datetime as dt
 import requests
 import discord
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 import emoji
 import re
 
@@ -20,42 +22,6 @@ async def displayResult(channel, result):
         await channel.send(f"**{result['msg']}**")
     else:
         await channel.send(result["msg"])
-
-
-@requires_paramaters(nb_parameters=5)
-@log_this
-async def addReminder(parameters, channel, cog=None):
-    """Adds a reminder in the database
-
-    Args:
-        parameters (list): The list of parameters required for the command to work
-        channel (discord.channel): The channel in which the command has been done
-        cog (Cog, optional): The cog which handles the periodic events. Defaults to None.
-    """
-    start_time = datetime.strptime(
-        "{} {}".format(parameters[0], parameters[1]), "%d/%m/%Y %H:%M"
-    )
-    name = parameters[2].lower()
-
-    hours, minutes = parameters[3].split(":")
-    duration = dt.timedelta(hours=int(hours), minutes=int(minutes))
-
-    people_to_remind = " ".join(parameters[4:])
-
-    start_time = timezone.make_aware(start_time)
-
-    await sync_to_async(createReminder)(
-        name=name,
-        start_time=start_time,
-        duration=duration,
-        people_to_remind=people_to_remind,
-        channel_id=channel.id,
-        server_id=channel.guild.id,
-    )
-    message = "Bert a ajouté l'évenement **{}** le **{}** (pour {})".format(
-        name, start_time.strftime("%d/%m/%Y à %H:%M"), people_to_remind
-    )
-    await channel.send(message)
 
 
 @requires_paramaters
@@ -287,35 +253,3 @@ async def vote(parameters, channel, cog=None):
 
     for i in range(len(responses)):
         await message.add_reaction(emoji.emojize(word_emojis[i]))
-
-
-commands = {
-    "addreminder": addReminder,
-    "delreminder": delReminder,
-    "modreminder": modReminder,
-    "getfuture": getFuture,
-    "morsty": morsty,
-    "deathping": deathping,
-    "stopping": stopping,
-    "help": hjelp,
-    "pic": pic,
-    "vote": vote,
-}
-
-
-@log_this
-async def execCommand(line, channel, cog):
-    """Executes the given command if it exists
-
-    Args:
-        line (str): The original message content
-        channel (discord.channel): The channel in which the command has been done
-        cog (Cog): The cog which handles the periodic events.
-    """
-    parameters = (
-        line.replace("\n", " ").replace("\r", " ").replace("\t", " ").split(" ")
-    )
-    if parameters[0] not in commands.keys():
-        await channel.send("Bert pas connaitre `{}`".format(parameters[0]))
-        return
-    await commands[parameters[0]](parameters[1:], channel, cog)
