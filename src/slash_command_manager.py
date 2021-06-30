e12df53f1d6df4d0dfb9aad56d669f7810d07642
Minigrim0 <grimauflorent@gmail.com>
2021-06-30 15:04:14 +0200

    [Add] slash command manager

---
+++ src/slash_command_manager.py
@@ -0,0 +1,49 @@
+from discord_slash import SlashContext
+from discord_slash.utils.manage_commands import create_option
+
+from singleton.client import Bert
+from singleton.command_manager import CommandManager
+
+slash = Bert.getInstance().slash
+
+
+@slash.slash(
+    name="addreminder",
+    description="creates a reminder at the given date that concerns the mentionned role/user",
+    guild_ids=[789136699477065748],
+    options=[
+        create_option(
+            name="date",
+            description="The date of the reminder",
+            required=True,
+            option_type=3
+        ),
+        create_option(
+            name="hour",
+            description="The hour of the reminder",
+            required=True,
+            option_type=3
+        ),
+        create_option(
+            name="name",
+            description="The name of the reminder",
+            required=True,
+            option_type=3
+        ),
+        create_option(
+            name="duration",
+            description="The duration of the reminder",
+            required=True,
+            option_type=3
+        ),
+        create_option(
+            name="peopleToRemind",
+            description="The people concerned by this reminder",
+            required=True,
+            option_type=6
+        )
+    ]
+)
+async def _addreminder(ctx: SlashContext, date: str, hour: str, name: str, duration: str, peopleToRemind: str):
+    params = [date, hour, name, duration, peopleToRemind]
+    CommandManager.getInstance().addReminder(params, ctx)
