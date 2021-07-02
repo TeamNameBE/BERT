e24dacc70c5657162756e85a1411b0b66b3bb65d
Minigrim0 <grimauflorent@gmail.com>
2021-07-02 10:52:57 +0200

    [Add] documentation

--- singleton/client.py
+++ singleton/client.py
@@ -1,6 +1,5 @@
 import discord
 
-from src.cog import ReminderCog
 from discord_slash import SlashCommand
 from singleton.command_manager import CommandManager
 
@@ -10,6 +9,11 @@ class Bert(discord.Client):
 
     @staticmethod
     def getInstance():
+        """Returns the instance of the singleton
+
+        Returns:
+            Bert: The instance
+        """
         if Bert.instance is None:
             Bert()
         return Bert.instance
@@ -24,12 +28,23 @@ class Bert(discord.Client):
         self.slash = SlashCommand(self, sync_commands=True)
         self.commandManager = CommandManager(self)
 
-    async def on_ready(self):
+    async def on_ready(self) -> None:
+        """Function called when the bot is connected to the API"""
         print('Logged on as {0}!'.format(self.user))
 
-    async def on_message(self, message):
+    async def on_message(self, message: discord.Message) -> None:
+        """Called when a message is sent womewhere the bot can access
+
+        Args:
+            message (discord.Message): The message with its metadata
+        """
         if message.content and message.content[0] == "/":
             await self.commandManager.execCommand(message.content[1:], message.channel)
 
-    def run(self, token):
+    def run(self, token: str) -> None:
+        """Sets the bot as only and ready to receive messages
+
+        Args:
+            token (str): The secret token used to interact with discord API
+        """
         super().run(token)
